a8dcdb7e5aac26aa111d4a436f0afeaf03dcf074
Luka Rekhviashvili <rekhviashvili.luka125@gmail.com>
2024-07-04 18:32:17 +0400

    alter vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        index: '/src/index.html', // Assuming your main entry point is index.html
+        home: '/src/Home.js',     // Replace with the actual path to your Home component
+        signup: '/src/SignupPage.js', // Replace with the actual path to your SignupPage component
+        login: '/src/LoginPage.js',   // Replace with the actual path to your LoginPage component
+        contactus: '/src/ContactUsPage.js' // Replace with the actual path to your ContactUsPage component
+        // Add more entry points as needed
+      }
+    }
+  }
+});
